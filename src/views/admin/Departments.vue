43daf5daaff274c5692228479cfb274a28b66ef6
renzo <r.zegers@student.utwente.nl>
2025-06-30 16:22:45 +0200

    departments now load by default

--- src/views/admin/Departments.vue
+++ src/views/admin/Departments.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useAdminStore } from '../../stores/admin';
 
 const adminStore = useAdminStore();
